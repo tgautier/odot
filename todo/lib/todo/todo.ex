41940629fe65accb3bddfc87d3fcecac2c46ad36
Thomas Gautier <thomas@gautier.gg>
2019-04-09 14:40:25 +0200

    Finish public API

--- todo/lib/todo/todo.ex
+++ todo/lib/todo/todo.ex
@@ -1,2 +1,26 @@
 defmodule Todo do
+  def create(params) do
+    Todo.Workflows.CreateTask.run(%{
+      input: %{
+        description: params[:description],
+        reminder_in_days: params[:reminder_in_days]
+      }
+    })
+  end
+
+  def toggle(id) do
+    Todo.Workflows.ToggleTaskDone.run(%{
+      input: %{id: id}
+    })
+  end
+
+  def update(id, params) do
+    Todo.Workflows.UpdateTask.run(%{
+      input: %{
+        id: id,
+        description: params[:description],
+        reminder_in_days: params[:reminder_in_days]
+      }
+    })
+  end
 end
--- todo/lib/todo/workflows/update_task.ex
+++ todo/lib/todo/workflows/update_task.ex
@@ -9,12 +9,12 @@ defmodule Todo.Workflows.UpdateTask do
   ]
 
   def run(payload) do
-    reminder_in_seconds = payload.input.reminder_in_days * @day_in_seconds
-
-    params = %{
-      description: payload.input.description,
-      reminder_at: NaiveDateTime.add(NaiveDateTime.utc_now(), reminder_in_seconds, :second)
-    }
+    params =
+      %{
+        description: payload.input[:description],
+        reminder_at: date_from_days(payload.input[:reminder_in_days])
+      }
+      |> Todo.Map.remove_if_nil([:description, :reminder_at])
 
     task = Todo.Queries.Task.by_id(payload.input.id)
 
@@ -26,6 +26,15 @@ defmodule Todo.Workflows.UpdateTask do
     else
       {:error, "Error: can't find task_id #{payload.input.id}"}
     end
+  end
+
+  defp date_from_days(nil) do
+    nil
+  end
+
+  defp date_from_days(reminder_in_days) do
+    reminder_in_seconds = reminder_in_days * @day_in_seconds
+    NaiveDateTime.add(NaiveDateTime.utc_now(), reminder_in_seconds, :second)
   end
 
   defp to_struct(task) do
